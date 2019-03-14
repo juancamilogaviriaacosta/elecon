2100fc0948dc1ea42d5061f1e1dbd0c6ac9ce689
juan <juancamilogaviriaacosta@gmail.com>
2019-03-14 12:15:07 -0500

    commit juan 2019-03-14_12:15

--- elecon/urls.py
+++ elecon/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from sitio import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^index/$', views.index, name='index'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
